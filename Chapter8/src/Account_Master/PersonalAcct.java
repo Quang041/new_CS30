4643d198585acbad277e5c61f17b5ad52b493f5f
Quang041 <nguyenquangn68@gmail.com>
2026-05-04 02:52:05 -0600

    2:52 AM

--- Chapter8/src/Account_Master/PersonalAcct.java
+++ Chapter8/src/Account_Master/PersonalAcct.java
@@ -6,22 +6,23 @@ public class PersonalAcct extends Account {
 	
 	private double Personal_Acct;
 	public PersonalAcct(double bal, String fName, String lName, String st, String city1, String pro, String postal1) {
-		super(bal, fName, lName, st, city1, pro, postal1);
-		Personal_Acct = bal;
+		super(bal, fName, lName, st, city1, pro, postal1); //Calls the parents Account constructor to initialize balance and customer information//
 	}
-	
+
+	//Deposits the given amount by delegating directly to the parents deposit method//
 	public void deposit(double amt) {
-		super.deposit(amt);
+		super.deposit(amt); //Use the deposit method from the parent class to update to balance//
 	}
-	
+
+	//Withdraws the given amount and charge $2 after the balance falls under $100//
 	public void withdrawal(double amt) {
 	 	if (amt <= getBalance()) {
-           super.withdrawal(amt); // use parent's withdrawal
+           super.withdrawal(amt); // use parent's withdrawal to withdraw that specific amount//
            if (getBalance() < 100) {
-               super.withdrawal(2); // charge $2 fee
+               super.withdrawal(2); // charge extra $2 if the balance falls under $100 after the transaction//
            }
        } else {
-           System.out.println("Not enough money in account.");
+           System.out.println("Not enough money in account."); //Notify user if the balance is not available for the withdraw//
        }
     }
 	
@@ -31,7 +32,7 @@ public class PersonalAcct extends Account {
 	}
 	
 	public String toString() {
-		return(super.toString());
+		return(super.toString()); //Call the toString method from the parent class//
 	}
 	
 	
