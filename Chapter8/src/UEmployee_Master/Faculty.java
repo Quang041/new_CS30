cd2226e7b7c0c3fe0a8bcb86656973d75e179616
Quang041 <nguyenquangn68@gmail.com>
2026-05-04 03:49:46 -0600

    3:49 AM

--- Chapter8/src/UEmployee_Master/Faculty.java
+++ Chapter8/src/UEmployee_Master/Faculty.java
@@ -2,15 +2,18 @@ package UEmployee_Master;
 
 public class Faculty extends UEmployee {
 
-	private String workplace;
+	private String workplace;//Stores the faculty member's current workplace//
+	//Constructor: passes name and salary up to the parent UEmployee constructor//
 	public Faculty(String name, double salary) {
-		super(name,salary);
+		super(name,salary); //Calls UEmployee's constructor to initialize name and salary//
 	}
-	
+
+	//Sets the faculty member's workplace to the provided value//
 	public void setFaculty(String w) {
 		workplace = w;
 	}
-	
+
+	//Returns a formatted string displaying the faculty member's name, salary, and workplace//
 	public String getFaculty() {
 		String work = getName() + ", income $" + getSalary() + " currently working at " + workplace;
 		return work;
